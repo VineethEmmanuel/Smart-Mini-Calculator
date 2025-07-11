8ea50affb093a4dca175f49c6eec50294addecbb
Vineeth Pilli <67647805+VineethEmmanuel@users.noreply.github.com>
2025-07-12 02:04:32 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono, Montserrat, Montserrat } from "next/font/google";
+import { Geist, Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
